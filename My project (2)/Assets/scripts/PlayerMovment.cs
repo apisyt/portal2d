06f52ce0158d68267c05dd291dcf4bd74bef968e
apisyt <86847964+apisyt@users.noreply.github.com>
2024-12-10 17:05:08 +0100

    duble jump itp

--- My project (2)/Assets/scripts/PlayerMovment.cs
+++ My project (2)/Assets/scripts/PlayerMovment.cs
@@ -3,21 +3,38 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
-    private float speed = 8f;
-    private float jumpingPower = 16f;
+    [SerializeField] private float speed = 8f;
+    [SerializeField] private float jumpingPower = 16f;
     private bool isFacingRight = true;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
+    private int jumpCount = 0;       // Licznik skoków
+    [SerializeField] private int maxJumps = 2;  // Maksymalna liczba skoków
+    private bool isGrounded;         // Czy gracz dotyka ziemi
+
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        // Sprawdzanie czy gracz dotyka ziemi
+        if (IsGrounded() && !isGrounded)
+        {
+            isGrounded = true;
+            jumpCount = 0; // Reset skoków tylko raz po dotkniêciu ziemi
+        }
+        else if (!IsGrounded())
+        {
+            isGrounded = false;
+        }
+
+        // Logika skakania
+        if (Input.GetButtonDown("Jump") && jumpCount < maxJumps)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            jumpCount++; // Zwiêksz licznik skoków
         }
 
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
