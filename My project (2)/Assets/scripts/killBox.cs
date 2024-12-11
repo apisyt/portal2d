817841e97ce3a2e3470f4bf007929a4620c158c7
apisyt <86847964+apisyt@users.noreply.github.com>
2024-12-11 23:08:57 +0100

    update misalenius

--- My project (2)/Assets/scripts/killBox.cs
+++ My project (2)/Assets/scripts/killBox.cs
@@ -2,17 +2,20 @@ using UnityEngine;
 
 public class TeleportPlayer2D : MonoBehaviour
 {
+    // Publiczne pole, które pozwala na ustawienie pozycji teleportacji w inspektorze
+    [SerializeField] private Vector3 teleportPosition = new Vector3(0, 0, 0);
+
     // Funkcja wywo³ywana, gdy inny Collider2D wejdzie w trigger
     private void OnTriggerEnter2D(Collider2D other)
     {
         // Sprawdzamy, czy obiekt, który wszed³ w trigger, ma tag "Player"
         if (other.CompareTag("Player"))
         {
-            // Teleportujemy gracza do pozycji (0, 0, 0)
-            other.transform.position = new Vector3(0, 0, 0);
+            // Teleportujemy gracza do ustawionej pozycji
+            other.transform.position = teleportPosition;
 
             // Wypisujemy komunikat do konsoli (opcjonalnie)
-            Debug.Log("Gracz zosta³ teleportowany do (0, 0, 0)!");
+            Debug.Log($"Gracz zosta³ teleportowany do {teleportPosition}!");
         }
     }
 }
