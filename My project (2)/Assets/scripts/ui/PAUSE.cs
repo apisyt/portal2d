c0e85cf814c6b16e5b977828e6b2a463402854e9
apisyt <86847964+apisyt@users.noreply.github.com>
2025-04-24 22:40:26 +0200

    paus menu

---
+++ My project (2)/Assets/scripts/ui/PAUSE.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pausePanel;
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        pausePanel.SetActive(true);
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        pausePanel.SetActive(false);
+        isPaused = false;
+    }
+}
